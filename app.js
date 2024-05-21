e1a3db3b791abbe55bcc7158181d75a4d4ca56a4
VDuck02 <88956193+VDuck02@users.noreply.github.com>
2024-05-21 15:20:34 +0700

    update core code

--- app.js
+++ app.js
@@ -1,41 +1,92 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cors = require('cors');
+const fs = require('fs');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const server = require('http').createServer(app);
+var io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"]
+    }
+})
+
+const serverPort = 3002;
+
+
+var user_socket_connect_list = [];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({extended: true, limit: '100mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+const corsOptions = {
+    origin: "http://localhost:4200",
+}
+
+app.use(cors(corsOptions));
+
+fs.readdirSync('./controllers').forEach((file) => {
+    if (file.substr(-3) === ".js") {
+        const route = require('./controllers/' + file);
+        if (typeof route.controller === 'function') {
+            route.controller(app, io, user_socket_connect_list);
+        }
+    }
+})
+
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
+
+server.listen(serverPort);
+
+console.log("Server Start : " + serverPort);
+
+Array.prototype.swap = (x, y) => {
+    const b = this[x];
+    this[x] = this[y];
+    this[y] = b;
+    return this;
+}
+
+Array.prototype.insert = (index, item) => {
+    this.splice(index, 0, item);
+}
+
+Array.prototype.replace_null = (replace = '""') => {
+    return JSON.parse(JSON.stringify(this).replace(/mull/g, replace));
+}
+
+String.prototype.replaceAll = (search, replacement) => {
+    const target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+}
---
+++ controllers/login_controller.js
@@ -0,0 +1,135 @@
+const db = require('./../helpers/db_helpers');
+const helper = require('./../helpers/helpers');
+const multiparty = require('multiparty'); //npm i multiparty
+const fs = require('fs');
+const imageSavePath = "./public/img/";
+
+
+module.exports.controller = (app, io, socket_list ) => {
+
+    const msg_success = "successfully";
+    const msg_fail = "fail";
+    const msg_invalidUser = "invalid username and password";
+
+    app.post('/api/login', (req, res) => {
+        helper.Dlog(req.body);
+        var reqObj =  req.body;
+
+        helper.CheckParameterValid(res, reqObj, ["email", "password"], () => {
+
+            db.query('SELECT `user_id`, `email`, `created_date`, `update_date`, `status` FROM `user_detail` WHERE `email` = ? AND  `password` = ? AND `status` = "1" ', [ reqObj.email, reqObj.password ] , (err, result) => {
+
+                if(err) {
+                    helper.ThrowHtmlError(err, res);
+                    return
+                }
+
+                if(result.length > 0) {
+                    res.json({ "status": "1", "payload": result[0] , "message": msg_success })
+                }else{
+                    res.json({ "status": "0", "message": msg_invalidUser })
+                }
+            })
+        } )
+    } )
+
+    app.post('/api/upload_image', (req, res) => {
+        const form = new multiparty.Form();
+        form.parse(req, (err, reqObj, files) => {
+            if(err) {
+                helper.ThrowHtmlError(err, res);
+                return;
+            }
+
+            helper.Dlog("--------------- Parameter --------------")
+            helper.Dlog(reqObj);
+
+            helper.Dlog("--------------- Files --------------")
+            helper.Dlog(files);
+
+            if(files.image !== undefined ) {
+                const extension = files.image[0].originalFilename.substring(files.image[0].originalFilename.lastIndexOf(".") + 1);
+                const imageFileName = helper.fileNameGenerate(extension);
+
+                const newPath = imageSavePath + imageFileName;
+
+                fs.rename(files.image[0].path, newPath, (err) => {
+
+                    if(err) {
+                        helper.ThrowHtmlError(err);
+                        return;
+                    }else{
+
+                        const name = reqObj.name;
+                        const address = reqObj.address;
+
+                        helper.Dlog(name);
+                        helper.Dlog(address);
+
+                        res.json({
+                            "status":"1",
+                            "payload": {"name": name, "address": address, "image":  helper.ImagePath() + imageFileName },
+                            "message": msg_success
+                        })
+                    }
+                })
+            }
+        })
+    })
+
+    app.post('/api/upload_multi_image', (req, res) => {
+        var form = new multiparty.Form();
+        form.parse(req, (err, reqObj, files) => {
+            if (err) {
+                helper.ThrowHtmlError(err, res);
+                return;
+            }
+
+            helper.Dlog("--------------- Parameter --------------")
+            helper.Dlog(reqObj);
+
+            helper.Dlog("--------------- Files --------------")
+            helper.Dlog(files);
+
+            if (files.image !== undefined) {
+
+                var imageNamePathArr = []
+                var fullImageNamePathArr = [];
+                files.image.forEach( imageFile => {
+                    const extension = imageFile.originalFilename.substring(imageFile.originalFilename.lastIndexOf(".") + 1);
+                    const imageFileName = helper.fileNameGenerate(extension);
+
+                    imageNamePathArr.push(imageFileName);
+                    fullImageNamePathArr.push(helper.ImagePath() + imageFileName);
+                    saveImage(imageFile, imageSavePath + imageFileName );
+                });
+
+                helper.Dlog(imageNamePathArr);
+                helper.Dlog(fullImageNamePathArr);
+
+                const name = reqObj.name;
+                const address = reqObj.address;
+
+                helper.Dlog(name);
+                helper.Dlog(address);
+
+
+                res.json({
+                    "status": "1",
+                    "payload": { "name": name, "address": address, "image": fullImageNamePathArr },
+                    "message": msg_success
+                })
+            }
+        })
+    })
+}
+
+function saveImage(imageFile, savePath ) {
+    fs.rename(imageFile.path, savePath, (err) => {
+
+        if (err) {
+            helper.ThrowHtmlError(err);
+            return;
+        }
+    })
+}
