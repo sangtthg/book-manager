b3649c98519cf455baf746890128d0ba01663e4e
trieusang <135527744+sangtthg@users.noreply.github.com>
2024-06-28 14:57:42 +0700

    commit code

--- app.js
+++ app.js
@@ -1,68 +1,69 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const cors = require('cors');
-const fs = require('fs');
+const cors = require("cors");
+const fs = require("fs");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const adminRouter = require("./routes/admin");
 
 const app = express();
-const server = require('http').createServer(app);
-var io = require('socket.io')(server, {
-    cors: {
-        origin: "http://localhost:4200",
-        methods: ["GET", "POST"]
-    }
-})
+const server = require("http").createServer(app);
+var io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+  },
+});
 
 const serverPort = 3002;
-
 
 var user_socket_connect_list = [];
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(logger('dev'));
-app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({extended: true, limit: '100mb'}));
+app.use(logger("dev"));
+app.use(logger("dev"));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/admin", adminRouter);
+app.use("/users", usersRouter);
 
 const corsOptions = {
-    origin: "http://localhost:4200",
-}
+  origin: "http://localhost:4200",
+};
 
 app.use(cors(corsOptions));
 
-fs.readdirSync('./controllers').forEach((file) => {
-    if (file.substr(-3) === ".js") {
-        const route = require('./controllers/' + file);
-        if (typeof route.controller === 'function') {
-            route.controller(app, io, user_socket_connect_list);
-        }
+fs.readdirSync("./controllers").forEach((file) => {
+  if (file.substr(-3) === ".js") {
+    const route = require("./controllers/" + file);
+    if (typeof route.controller === "function") {
+      route.controller(app, io, user_socket_connect_list);
     }
-})
-
-app.use(function(req, res, next) {
-    next(createError(404));
+  }
 });
 
-app.use(function(err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
-    res.status(err.status || 500);
-    res.render('error');
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
@@ -72,21 +73,21 @@ server.listen(serverPort);
 console.log("Server Start : " + serverPort);
 
 Array.prototype.swap = (x, y) => {
-    const b = this[x];
-    this[x] = this[y];
-    this[y] = b;
-    return this;
-}
+  const b = this[x];
+  this[x] = this[y];
+  this[y] = b;
+  return this;
+};
 
 Array.prototype.insert = (index, item) => {
-    this.splice(index, 0, item);
-}
+  this.splice(index, 0, item);
+};
 
 Array.prototype.replace_null = (replace = '""') => {
-    return JSON.parse(JSON.stringify(this).replace(/mull/g, replace));
-}
+  return JSON.parse(JSON.stringify(this).replace(/mull/g, replace));
+};
 
 String.prototype.replaceAll = (search, replacement) => {
-    const target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-}
+  const target = this;
+  return target.replace(new RegExp(search, "g"), replacement);
+};
