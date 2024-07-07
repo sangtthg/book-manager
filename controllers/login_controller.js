eeb926e6ccdea120304e486f3b0b9e96ffba8392
Sang <trieusangqb03@gmail.com>
2024-07-07 21:47:10 +0700

    feat: hash password

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -2,8 +2,10 @@ const db = require("./../helpers/db_helpers");
 const helper = require("./../helpers/helpers");
 const multiparty = require("multiparty"); //npm i multiparty
 const fs = require("fs");
-const { sendOTPEmail } = require("../helpers/email_helpers");
+const { sendOTPEmail, verifyOTP } = require("../helpers/email_helpers");
 const jwt = require("../Service/jwt");
+const { comparePassword, hashPassword } = require("../Service/bcrypt");
+const { log } = require("console");
 module.exports.controller = (app, io, socket_list) => {
   const msg_success = "successfully";
   const msg_fail = "fail";
@@ -15,44 +17,105 @@ module.exports.controller = (app, io, socket_list) => {
 
     helper.CheckParameterValid(res, reqObj, ["email", "password"], () => {
       db.query(
-        'SELECT `user_id`, `email`, `created_date`, `user_status` FROM `users` WHERE `email` = ? AND  `password` = ? AND `user_status` = "1" ',
-        [reqObj.email, reqObj.password],
-        (err, result) => {
+        'SELECT * FROM `users` WHERE `email` = ? AND `user_status` = "1"',
+        [reqObj.email],
+        async (err, result) => {
           if (err) {
             helper.ThrowHtmlError(err, res);
             return;
           }
 
-          if (result.length > 0) {
-            res.json({ status: "1", data: result[0], message: msg_success });
-          } else {
+          if (result.length < 0) {
             res.json({ status: "0", message: msg_invalidUser });
+          }
+          try {
+            const user = result[0];
+            const compare = await comparePassword(
+              reqObj.password,
+              user.password
+            );
+            console.log("compare", compare);
+            if (!compare) {
+              return res.json({ status: "0", message: msg_invalidUser });
+            }
+            const obj = {
+              user_id: user.user_id,
+              role: user.role,
+              created_at: Date.now(),
+            };
+            const token = jwt.sign(obj);
+            if (!token) {
+              return res.json({ status: "0", message: msg_fail });
+            }
+            delete user.password;
+            return res.json({
+              status: "1",
+              data: {
+                user: user,
+                token: token,
+              },
+              message: msg_success,
+            });
+          } catch (error) {
+            console.log("error", error);
+            return res.json({ status: "0", message: msg_fail });
           }
         }
       );
     });
   });
 
+  //SEND OTP
+  app.post("/api/send_otp", (req, res) => {
+    helper.Dlog(req.body);
+    const reqObj = req.body;
+    helper.CheckParameterValid(res, reqObj, ["email"], async () => {
+      try {
+        await sendOTPEmail(reqObj.email);
+        res.json({ status: "1", message: msg_success });
+      } catch (error) {
+        res.json({ status: "0", message: msg_fail });
+      }
+    });
+  });
+
   app.post("/api/register", (req, res) => {
     helper.Dlog(req.body);
-    const { email, password, re_password, verify } = req.body;
+    const reqObj = req.body;
+    helper.CheckParameterValid(
+      res,
+      reqObj,
+      ["email", "password", "re_password", "verify", "username"],
+      async () => {
+        const { username, email, password, re_password, verify } = reqObj;
+        const { otp_id, otp } = verify;
+        if (password !== re_password) {
+          return res.json({ status: "0", message: "Mật khẩu không khớp" });
+        }
+        const _verifyOTP = await verifyOTP(otp_id, email, otp);
+        if (!_verifyOTP) {
+          return res.json({ status: "0", message: "Mã OTP không hợp lệ" });
+        }
+        db.query(
+          "SELECT `user_id`, `email`, `created_at`, `user_status` FROM `users` WHERE `email` = ?",
+          [reqObj.email],
+          async (err, result) => {
+            if (err) {
+              helper.ThrowHtmlError(err, res);
+              return;
+            }
 
-    helper.CheckParameterValid(res, reqObj, ["email", "password"], () => {
-      db.query(
-        "SELECT `user_id`, `email`, `created_date`, `user_status` FROM `users` WHERE `email` = ?",
-        [reqObj.email],
-        (err, result) => {
-          if (err) {
-            helper.ThrowHtmlError(err, res);
-            return;
-          }
+            if (result.length > 0) {
+              return res.json({ status: "0", message: "Email đã tồn tại" });
+            }
+            const passwordCrypt = await hashPassword(password);
 
-          if (result.length > 0) {
-            res.json({ status: "0", message: "Email đã tồn tại" });
-          } else {
+            if (!passwordCrypt) {
+              return res.json({ status: "0", message: "Lỗi mã hóa mật khẩu" });
+            }
             db.query(
-              'INSERT INTO `users` (`email`, `password`, `user_status`) VALUES (?, ?, "1")',
-              [reqObj.email, reqObj.password],
+              'INSERT INTO `users` (`email`, `password`, `username`, `user_status`, `created_at`, `updated_at`) VALUES (?, ?, ?, "1", NOW(), NOW())',
+              [reqObj.email, passwordCrypt, reqObj.username],
               (err, result) => {
                 if (err) {
                   helper.ThrowHtmlError(err, res);
@@ -63,9 +126,9 @@ module.exports.controller = (app, io, socket_list) => {
               }
             );
           }
-        }
-      );
-    });
+        );
+      }
+    );
   });
 
   //ADMIN
@@ -75,18 +138,26 @@ module.exports.controller = (app, io, socket_list) => {
 
     helper.CheckParameterValid(res, reqObj, ["email", "password"], () => {
       db.query(
-        'SELECT `user_id`, `email`, `created_at`, `user_status`, `role`  FROM `users` WHERE `email` = ? AND  `password` = ? AND `user_status` = "1" AND (`role` = "admin" OR `role` = "member")',
+        'SELECT * FROM `users` WHERE `email` = ? AND  `password` = ? AND `user_status` = "1" AND (`role` = "admin" OR `role` = "member")',
         [reqObj.email, reqObj.password],
         async (err, result) => {
           if (err) {
             helper.ThrowHtmlError(err, res);
             return;
           }
+
           if (result.length < 0) {
-            return res.json({ status: "0", message: msg_invalidUser });
+            res.json({ status: "0", message: msg_invalidUser });
           }
           try {
             const user = result[0];
+            const compare = await comparePassword(
+              reqObj.password,
+              user.password
+            );
+            if (!compare) {
+              return res.json({ status: "0", message: msg_invalidUser });
+            }
             const obj = {
               user_id: user.user_id,
               role: user.role,
@@ -96,7 +167,7 @@ module.exports.controller = (app, io, socket_list) => {
             if (!token) {
               return res.json({ status: "0", message: msg_fail });
             }
-
+            delete user.password;
             return res.json({
               status: "1",
               data: {
