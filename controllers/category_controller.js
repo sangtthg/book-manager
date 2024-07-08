88749cc228a366a7e66314076dc51b50f7e39f9c
Sang <trieusangqb03@gmail.com>
2024-07-08 21:36:08 +0700

    fix: category controller

--- controllers/category_controller.js
+++ controllers/category_controller.js
@@ -8,6 +8,27 @@ const msg_success = "successfully";
 const msg_fail = "fail";
 const msg_invalidUser = "invalid username and password";
 
+const checkUniqueName = (name) => {
+  return new Promise((resolve, reject) => {
+    const query = `SELECT * FROM categories WHERE category_name = '${formatName(
+      name
+    )}'`;
+    db.query(query, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      if (result.length > 0) {
+        resolve(false);
+      }
+      resolve(true);
+    });
+  });
+};
+
+const formatName = (name) => {
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+};
+
 module.exports.controller = (app, io, socket_list) => {
   app.post("/api/category/add", helper.authorization, (req, res) => {
     helper.CheckParameterValid(res, req.body, ["name"], async () => {
@@ -25,8 +46,16 @@ module.exports.controller = (app, io, socket_list) => {
           message: "Category name is required",
         });
       }
-
-      const query = `INSERT INTO categories (category_name, created_by) VALUES ('${req.body.name}', '${req.auth.user_id}')`;
+      const checkName = await checkUniqueName(req.body.name);
+      if (!checkName) {
+        return res.json({
+          status: "0",
+          message: "Category name đã tồn tại",
+        });
+      }
+      const query = `INSERT INTO categories (category_name, created_by) VALUES ('${formatName(
+        req.body.name
+      )}', '${req.auth.user_id}')`;
       db.query(query, (err, result) => {
         if (err) {
           return res.json({
@@ -62,8 +91,17 @@ module.exports.controller = (app, io, socket_list) => {
             message: "Category name is required",
           });
         }
+        const checkName = await checkUniqueName(req.body.name);
+        if (!checkName) {
+          return res.json({
+            status: "0",
+            message: "Category name đã tồn tại",
+          });
+        }
 
-        const query = `UPDATE categories SET category_name = '${req.body.name}' WHERE category_id = '${req.body.category_id}'`;
+        const query = `UPDATE categories SET category_name = '${formatName(
+          req.body.name
+        )}' WHERE category_id = '${req.body.category_id}'`;
         db.query(query, (err, result) => {
           if (err) {
             return res.json({
