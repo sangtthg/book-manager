289aeb724acd2e7862fb67877987edd0f7ad65b1
trieusang <135527744+sangtthg@users.noreply.github.com>
2024-07-14 13:39:57 +0700

    feat: login

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,28 @@
 var express = require("express");
 var router = express.Router();
 var path = require("path");
-const home = path.join(__dirname, "../views/home.html");
+const login = path.join(__dirname, "../views/login.ejs");
+const home = path.join(__dirname, "../views/home.ejs");
+const ListProduct = path.join(__dirname, "../views/ListProduct.ejs");
+const Account = path.join(__dirname, "../views/Account.ejs");
+const Catelogy = path.join(__dirname, "../views/Catelogy.ejs");
 
 router.get("/home", function (req, res, next) {
-  res.render(home, { title: "home" });
+  res.render(home);
 });
 
+router.get("/", function (req, res, next) {
+  res.render(login);
+});
+router.get("/ListProduct", function (req, res, next) {
+  res.render(ListProduct);
+});
+router.get("/Account", function (req, res, next) {
+  res.render(Account);
+});
+
+router.get("/Catelogy", function (req, res, next) {
+  console.log("cabscacakjhbckajsbk");
+  res.render(Catelogy);
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,8 @@ var express = require("express");
 var path = require("path");
 var router = express.Router();
 const login = path.join(__dirname, "../views/login.html");
-router.get("/", function (req, res, next) {
-  res.sendFile(login);
-});
+// router.get("/", function (req, res, next) {
+//   res.sendFile(login);
+// });
 
-router.post("/api/admin/login", function (req, res) {
-  const { email, password } = req.body;
-
-  if (email === "admin@gmail.com" && password === "password") {
-    req.session.user = { email };
-    res.json({ status: "1" });
-  } else {
-    res.json({ status: "0" });
-  }
-});
 module.exports = router;
