bedc36a29395bab95495e1c61870a813a1b6d63a
Sang <trieusangqb03@gmail.com>
2024-07-17 19:44:38 +0700

    feat: user controller

---
+++ constants/common.js
@@ -0,0 +1,8 @@
+module.exports = {
+  avatarDefault:
+    "https://res.cloudinary.com/dqtrsuhez/image/upload/v1721100332/DATN/pqwas8oniuwomjyksp8z.png",
+  roles: ["admin", "user", "member"],
+  msg_success: "successfully",
+  msg_fail: "fail",
+  msg_invalidUser: "invalid username and password",
+};
