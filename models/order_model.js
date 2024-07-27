d6f24f553390a6b9306f9384ed989735aaeac6ce
VDuck02 <88956193+VDuck02@users.noreply.github.com>
2024-07-27 19:57:55 +0700

    fix bug

--- models/order_model.js
+++ models/order_model.js
@@ -3,67 +3,71 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../helpers/sequelize_helpers");
 
 const Order = sequelize.define(
-  "Order",
-  {
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    // cartId: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    // },
-    // status: {
-    //   type: DataTypes.STRING,
-    //   allowNull: false,
-    // },
-    quantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    totalPrice: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    paymentType: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "pending",
-    },
-    paymentStatus: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "pending",
-    },
-    orderStatus: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    voucherId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    discountPrice: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-    },
+    "Order",
+    {
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        // cartId: {
+        //   type: DataTypes.INTEGER,
+        //   allowNull: false,
+        // },
+        // status: {
+        //   type: DataTypes.STRING,
+        //   allowNull: false,
+        // },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        totalPrice: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        paymentType: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "pending",
+        },
+        paymentStatus: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "pending",
+        },
+        orderStatus: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        voucherId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        discountPrice: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
 
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        items: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        statusShip: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "pending",
+        },
+        listCartId: {
+            type: DataTypes.JSON,
+            allowNull: false,
+        },
     },
-    items: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    statusShip: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "pending",
-    },
-  },
-  {
-    tableName: "Orders",
-  }
+    {
+        tableName: "Orders",
+    }
 );
 module.exports = Order;
