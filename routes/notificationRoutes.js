c6e1ddaf14c693e6239a5a020914d570bd8bcab7
lindq09 <quanglinh@media-one.vn>
2024-07-27 11:47:50 +0700

    update notificationRoutes.js á á auth

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getNotificationsByUser, markAsRead, markAllAsRead } = require('../controllers/notificationController');
+const {
+  getNotificationsByUser,
+  markAsRead,
+  markAllAsRead,
+} = require("../controllers/notificationController");
+const helpers = require("../helpers/helpers");
 
-router.get('/notifications', getNotificationsByUser);
+router.get("/notifications", helpers.authorization, getNotificationsByUser);
 
-router.patch('/notifications/:notificationId/read', markAsRead);
+router.patch("/notifications/:notificationId/read", helpers.authorization, markAsRead);
 
-router.patch('/notifications/read-all', markAllAsRead);
+router.patch("/notifications/read-all", helpers.authorization, markAllAsRead);
 
 module.exports = router;
