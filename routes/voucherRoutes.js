ac8b5372c027bbd3849db83ef098e870529ac82d
tiendh2002 <duonghongtienfpoly@gmail.com>
2024-08-30 19:37:18 +0700

    Update api coupon

--- routes/voucherRoutes.js
+++ routes/voucherRoutes.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const voucherController = require('../controllers/voucherController');
+const voucherController = require("../controllers/voucherController");
 
-router.post('/create', voucherController.createVoucher);
-router.get('/', voucherController.getAllVouchers);
-router.post('/update/:id', voucherController.updateVoucher);
-router.get('/edit/:id', voucherController.editVoucher);
-router.post('/delete/:id', voucherController.deleteVoucher); 
-router.get('/search', voucherController.searchVouchers);
+router.post("/create", voucherController.createVoucher);
+router.get("/", voucherController.getAllVouchers);
+router.post("/update/:id", voucherController.updateVoucher);
+router.get("/edit/:id", voucherController.editVoucher);
+router.post("/delete/:id", voucherController.deleteVoucher);
+router.get("/search", voucherController.searchVouchers);
+router.get("/json", voucherController.getAllVouchersJson);
 module.exports = router;
